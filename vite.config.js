3001f97c38f844a8c64b8de4a6d642dcfc8549ad
itperson <772622542@qq.com>
2021-10-14 17:57:18 +0800

    feat:package

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+			'@assets': resolve(__dirname, 'src/assets'),
+			'@components': resolve(__dirname, 'src/components'),
+		},
+	},
   plugins: [vue()]
 })
